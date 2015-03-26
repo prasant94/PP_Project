2a7aa17673da63cb9974e8402c883bb62182ab13
Ishan <ishan12041@iiitd.ac.in>
2015-03-26 23:39:16 +0530

    Client skeleton made

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,40 @@
 package client;
 
-public class Client {
+import java.util.ArrayList;
 
+public class Client {
+	
+	ServerConnection serverConenction;
+	
+	protected Client(){
+		
+	}
+	
+	protected String browseMedia(){
+		return null;
+		
+	}
+	
+	protected boolean streamMedia(String filename){
+		return false;
+		
+	}
+	
+	
+	protected boolean downloadMedia(String filename){
+		return false;
+		
+	}
+	
+	protected boolean uploadMedia(String filename){
+		return false;
+	}
+	
+	protected ArrayList<String> searchMedia(String filename){
+		return null;
+		
+	}
+	
+	
+	
 }
