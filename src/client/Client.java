2260428278fbe6023faabad1b40830d63b034a0f
Prasant Chidella <prasant12075@iiitd.ac.in>
2015-03-26 23:41:18 +0530

    methods in some classes

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,8 @@
 package client;
 
 public class Client {
+	private String ip;
+	private int port;
+	private ServerConnection connection;
 
 }
--- src/client/ServerConnection.java
+++ src/client/ServerConnection.java
@@ -1,5 +1,45 @@
 package client;
 
 public class ServerConnection {
+	private String serverIp;
+	private int serverPort;
 
+	/**
+	 *
+	 */
+	public ServerConnection() {
+		// TODO
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public String getServerIp() {
+		return serverIp;
+	}
+
+	/**
+	 *
+	 * @param serverIp
+	 */
+	public void setServerIp(String serverIp) {
+		this.serverIp = serverIp;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public int getServerPort() {
+		return serverPort;
+	}
+
+	/**
+	 *
+	 * @param serverPort
+	 */
+	public void setServerPort(int serverPort) {
+		this.serverPort = serverPort;
+	}
 }
