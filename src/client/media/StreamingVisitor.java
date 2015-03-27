65fa69c3e94db39812fdfeb496b360f69765532f
Ishan <ishan12041@iiitd.ac.in>
2015-03-27 22:17:40 +0530

    StreamVisitor Updated

--- src/client/media/StreamingVisitor.java
+++ src/client/media/StreamingVisitor.java
@@ -1,5 +1,19 @@
 package client.media;
 
-public class StreamingVisitor {
+public class StreamingVisitor extends MediaVisitor{
 
+
+	@Override
+	boolean visitAudioMedia(AudioPlayer audioPlayer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean visitVideoMedia(VideoPlayer audioPlayer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 }
